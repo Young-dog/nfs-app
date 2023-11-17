2e8b4a05656f373da2772725e91c79ed7cf30f21
r.timerkhanov <rahimtoy@gmail.com>
2023-11-18 01:01:57 +0300

    Добавил мок данные для modules

---
+++ lib/src/shared/data/data_sources/module_data.dart
@@ -0,0 +1,10 @@
+List<Map<String, dynamic>> modules = [
+  {
+    'moduleId': '',
+    'moduleName': '',
+    'moduleCapacity': 200,
+    'creationDate': '11.18.2023 01:00',
+    'modification_date': '11.18.2023 01:00',
+    'status': true,
+  },
+];
