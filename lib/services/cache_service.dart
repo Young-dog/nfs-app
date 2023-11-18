f1d9f38ab4b752ab364ff7811934685af7d7330e
r.timerkhanov <rahimtoy@gmail.com>
2023-11-19 00:20:10 +0300

    Пробуем кещировать

--- lib/services/cache_service.dart
+++ lib/services/cache_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:connectivity/connectivity.dart';
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 class CacheService {
@@ -14,8 +15,10 @@ class CacheService {
     _connectivitySubscription = Connectivity()
         .onConnectivityChanged
         .listen((ConnectivityResult result) async {
-      if (result == ConnectivityResult.mobile ||
-          result == ConnectivityResult.wifi) {
+      if (result == ConnectivityResult.mobile || result == ConnectivityResult.wifi) {
+
+        print('------------------> Yeas!!! Got internet!!!');
+
         await _sendCachedData();
       }
     });
@@ -30,11 +33,18 @@ class CacheService {
   }
 
   Future<void> _sendCachedData() async {
+
+    debugPrint('--- ');
+
     final List<String> cachedData = List<String>.from(_box.values);
+
+    debugPrint('--- cachedData= ${cachedData.length}');
+
+
     if (cachedData.isNotEmpty) {
       // Implement your data sending logic here
       // For example, you can make a network request to send the data
-      print('-------> Sending cached data: $cachedData');
+      print('------------------> Sending cached data: $cachedData');
 
       // Once the data is sent successfully, you can clear the cache
       await _box.clear();
