9148d3694ab472ec5536875e271e3df188fa74db
r.timerkhanov <rahimtoy@gmail.com>
2023-11-18 12:38:32 +0300

    Добавил данные, модели, сущности для поля #4

--- lib/src/shared/data/data_sources/land_data.dart
+++ lib/src/shared/data/data_sources/land_data.dart
@@ -1,3 +1,8 @@
-Map<String, dynamic> landData = {
+import 'dart:math';
 
+Map<String, dynamic> landData = {
+  'landId': 'landId_1', // Уникальный идентификатор поля
+  'title': '', // Название поля
+  'square': 200, // Площадь
+  'coordinates': const Point(1.0, 1.0), // Координаты
 };
---
+++ lib/src/shared/data/data_sources/land_plant_condition_data.dart
@@ -0,0 +1,4 @@
+Map<String, dynamic> landPlantConditionData = {
+  'conditionId': 'conditionId', // Уникальный идентификатор сотояния растений
+  'landId': 'landId_1', // Референс на идентификатор поля
+};
