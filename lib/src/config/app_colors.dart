114432b0f4026fdb42ecc88091f90730a54e7f74
Maxim Terekhin <mm.terekhin@gmail.com>
2023-11-19 03:49:08 +0300

    Изменение Ui в авторизации

--- lib/src/config/app_colors.dart
+++ lib/src/config/app_colors.dart
@@ -7,10 +7,14 @@ class AppColors {
   static Color get greyishBlue => const Color(0xffB1C1DA);
   static Color get grey => const Color(0xff595d67);
   static Color get white => const Color(0xffffffff);
+  static Color get black => const Color(0xff000000);
+  static Color get bgPrimary => const Color(0xffFDFDFD);
+
 
   static Color get background => const Color(0xffF4F8FB);
 
-  static Color get green => const Color(0xff40CDAB);
+  static Color get green => const Color(0xff00AA71);
+  static Color get greenSecondary => const Color(0xFF026074);
   static Color get orange => const Color(0xffFB834A);
   static Color get red => const Color(0xffD42C37);
 }
--- lib/src/features/auth/presentation/screens/login_screen.dart
+++ lib/src/features/auth/presentation/screens/login_screen.dart
@@ -1,43 +1,92 @@
+import 'package:app/src/config/app_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../../../config/app_colors.dart';
 import '../../../../shared/widgets/custom_indicator.dart';
 import '../../../../shared/widgets/custom_logo.dart';
 import '../../../../shared/widgets/custom_widget.dart';
 import '../blocs/login_with_nfs/login_with_nfs_cubit.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  void initState() {
+    context.read<LoginWithNfsCubit>().signInWithNfc();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const CustomLogo(
-                height: 45,
-                width: 45,
-                fontSize: 32,
+    return BlocConsumer<LoginWithNfsCubit, LoginWithNfsState>(
+      buildWhen: (previous, current) {
+        return previous.status != current.status;
+      },
+      listener: (context, state) {
+        if (state.status == LoginWithNfsStatus.error) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(state.errorText.toString()),
+            ),
+          );
+        }
+        if (state.status == LoginWithNfsStatus.success) {
+          context.goNamed('home');
+        }
+
+        print('---> state.status= ${state.status}');
+
+        if (state.status == LoginWithNfsStatus.signUp) {
+          context.goNamed('signup');
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Stack(
+                    children: [
+                      AppIcons.bgNfcIcon(null, AppColors.green),
+                      Positioned(
+                        left: 6,
+                        child: AppIcons.nfsIcon(
+                          null,
+                          Colors.transparent,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 100),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 43),
+                    child: Text(
+                      'Включите NFS и приложите  карту к задней панели телефона',
+                      style: Theme
+                          .of(context)
+                          .textTheme
+                          .bodyLarge,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 20),
-              Text(
-                'Мобильное приложение механизатора',
-                style: Theme.of(context).textTheme.bodyLarge,
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 40),
-              const _LoginWithNfs(),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
@@ -66,7 +115,6 @@ class _LoginWithNfs extends StatelessWidget {
         print('---> state.status= ${state.status}');
 
         if (state.status == LoginWithNfsStatus.signUp) {
-
           context.goNamed('signup');
         }
       },
@@ -78,12 +126,9 @@ class _LoginWithNfs extends StatelessWidget {
         }
         return CustomButton(
           text: 'Войти с помощью NFC',
-          onPressed: () =>
-              context.read<LoginWithNfsCubit>().signInWithNfc(),
+          onPressed: () => context.read<LoginWithNfsCubit>().signInWithNfc(),
         );
       },
     );
   }
 }
-
-
